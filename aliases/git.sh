42cd030930ae65734135991cdd2ae99354c7b4b0
Menno Liefstingh <git@menno.ai>
2024-08-08 15:59:31 +0200

    feat: adds git-open shortcut, try it exclamationmark

--- aliases/git.sh
+++ aliases/git.sh
@@ -5,6 +5,8 @@ alias gau='git add -u'
 
 alias gc='git commit -m'
 
+alias go='git open' # requires git-open https://github.com/paulirish/git-open
+
 alias gp='git push'
 
 alias gnb='git checkout -b $branch_name'
